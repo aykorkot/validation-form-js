bed4b31bc8ab86fe4053f4eb972615bb24e35f07
aymen korkot <ay.korkot@feeling-group.com>
2016-12-18 14:57:47 +0000

    modif2

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,41 @@
 
 
 function validate () {
-	 var nom = document.forms["valForm"]["nom"].value;
-	 var prenom = document.forms["valForm"]["prenom"].value;
-	 var mdp = document.forms["valForm"]["motdepasse"].value;
-	 var erreur = document.getElementsByTagName('small').style.display="block";
-	
-    if ( (nom == "") || (prenom == "") || (mdp == "") ){
-       
+
+
+	var nom = document.forms["valForm"]["nom"].value;
+	var prenom = document.forms["valForm"]["prenom"].value;
+	var mdp = document.forms["valForm"]["motdepasse"].value;
+  
+
+    if (nom == ""){
+    	document.getElementById("c1").style.display= "Block" ;
+
+    }else{
+    	document.getElementById("c1").style.display= "none" ;
     }
+
+    if (prenom == ""){
+		document.getElementById("c2").style.display= "Block"  ;
+    }
+    else{
+    	document.getElementById("c2").style.display= "none" ;
+    }
+
+    if (mdp == "" ){
+		document.getElementById("c3").style.display= "Block" ;
+    }
+    else{
+    	document.getElementById("c3").style.display= "none" ;
+    }
+
+    if (mdp.indexOf("@") == -1) {
+    	document.getElementById("c4").style.display= "Block";
+    }else{
+    	document.getElementById("c4").style.display= "none";
+    }
+
+   document.location.href="confirmation.html";
+
+event.preventDefault();
 }
