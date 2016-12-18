bf990c428fcfa0ba9e723f342fb210ffb5848872
aymen korkot <ay.korkot@feeling-group.com>
2016-12-18 16:05:41 +0000

    modif3

--- js/scripts.js
+++ js/scripts.js
@@ -4,40 +4,55 @@
 
 function validate () {
 
+    error();
 
+    var siErreur = 0;
 	var nom = document.forms["valForm"]["nom"].value;
 	var prenom = document.forms["valForm"]["prenom"].value;
 	var mdp = document.forms["valForm"]["motdepasse"].value;
-  
+
 
     if (nom == ""){
-    	document.getElementById("c1").style.display= "Block" ;
+        document.getElementsByClassName("c1")[0].style.display= "block" ;
 
-    }else{
-    	document.getElementById("c1").style.display= "none" ;
+        siErreur = 1;
     }
 
     if (prenom == ""){
-		document.getElementById("c2").style.display= "Block"  ;
+		document.getElementById("c2").style.display= "block"  ;
+        siErreur = 1;
     }
-    else{
-    	document.getElementById("c2").style.display= "none" ;
-    }
-
+ 
     if (mdp == "" ){
-		document.getElementById("c3").style.display= "Block" ;
-    }
-    else{
-    	document.getElementById("c3").style.display= "none" ;
+		document.getElementById("c3").style.display= "block" ;
+        siErreur = 1;
     }
 
     if (mdp.indexOf("@") == -1) {
-    	document.getElementById("c4").style.display= "Block";
-    }else{
-    	document.getElementById("c4").style.display= "none";
+    	document.getElementById("c4").style.display= "block";
+        siErreur = 1;
+    }
+     for(i=0;i<=9;i++){
+        if(mdp.indexOf(i) == -1){
+            document.getElementById("c5").style.display= "sblock" ;
+            siErreur = 1;
+        }
     }
 
-   document.location.href="confirmation.html";
+    if(siErreur==1){
+         event.preventDefault();
+    }
+  
 
-event.preventDefault();
+function error(){
+    document.getElementsByClassName("c1")[0].style.display= "none" ;
+    document.getElementById("c2").style.display= "none" ;
+    document.getElementById("c3").style.display= "none" ;
+    document.getElementById("c4").style.display= "none";
+        document.getElementById("c5").style.display= "none";
+}
+
+
+
+
 }
